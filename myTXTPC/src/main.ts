ab9dfb48aa6a05a34eedec4c85d2829f2f310f7b
Zshuaixxx <z_20058@qq.com>
2024-07-24 17:21:41 +0800

    feat:项目基础配置

--- myTXTPC/src/main.ts
+++ myTXTPC/src/main.ts
@@ -6,6 +6,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+//引入默认样式文件
+import '@/styles/common.scss'
 const app = createApp(App)
 
 app.use(createPinia())
